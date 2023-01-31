92c46df1da93c0d7a15ed8c905d08fcc7b219886
RodrigoEliasP <rodrigo.e2003@gmail.com>
2023-01-31 07:52:14 -0300

    testing math operations and data structures

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,3 +1,37 @@
+fn quadratic_formula(a: f32, b:f32, c: f32) -> (f32, f32) {
+    let discriminant = b.powf(2.0) - 4.0 * a * c;
+    let x1 = (-b + discriminant.sqrt()) / (2.0 * a);
+    let x2 = (-b - discriminant.sqrt()) / (2.0 * a);
+    (x1, x2)
+}
+struct Owner{
+    first_name: String,
+    last_name: String,
+}
+struct Account {
+    owner: Owner,
+    balance: f64
+}
+
+fn test () {
+    let owner = Owner {
+        first_name: String::from("Rodrigo"),
+        last_name: String::from("Elias")
+    };
+    let account = Account {
+        owner,
+        balance: 3000.0
+    };
+    println!(
+        "Name {} last name {} balance {}", 
+        account.owner.first_name, 
+        account.owner.last_name, 
+        account.balance
+    );
+}
+
 fn main() {
-    println!("Olá senhoras e senhores");
+    let (x1, x2) = quadratic_formula(1.0, -3.0, 2.0);
+    println!("Quadratic formula roots are x1: {x1} and x2 {x2}");
+    test();
 }
