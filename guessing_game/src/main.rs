7fd1922d82b7c6079c61151e43f30741dafef70d
RodrigoEliasP <rodrigo.e2003@gmail.com>
2023-01-31 09:23:45 -0300

    input reading did

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,19 @@
+use::std::io;
+
+fn print_game_heading() {
+    println!("Welcome player, lets play the guessing game.");
+    println!("The game works just like this");
+    println!("first the game generates a number between 1 and 100");
+    println!("then you will be asket to input a number, if you hit the number generated you win");
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_game_heading();
+
+    let mut guess = String::new();
+
+    let result = io::stdin().read_line(&mut guess);
+    result.expect("Something went wring while trying to read the input");
+    
+    println!("You guessed: {guess}");
 }
