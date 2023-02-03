fca732b9170dd670ee162e0c3566bb9edfaec227
RodrigoEliasP <rodrigo.e2003@gmail.com>
2023-02-03 18:03:40 -0300

    -a

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,6 @@
-use::std::io;
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn print_game_heading() {
     println!("Welcome player, lets play the guessing game.");
@@ -10,10 +12,19 @@ fn print_game_heading() {
 fn main() {
     print_game_heading();
 
+    let secret_number = rand::thread_rng().gen_range(1..101);
     let mut guess = String::new();
 
     let result = io::stdin().read_line(&mut guess);
     result.expect("Something went wring while trying to read the input");
+
+    let guess: u8 = guess.trim().parse().expect("Please type a number between 1 and 100!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Equal => println!("You win!!!"),
+        Ordering::Less => println!("Smaller!"),
+        Ordering::Greater => println!("Bigger!"),
+    }
     
     println!("You guessed: {guess}");
 }
