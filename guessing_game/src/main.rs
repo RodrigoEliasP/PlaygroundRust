04d5ac7f1a9d1083f59534175a2227b8d8738e58
RodrigoEliasP <rodrigo.e2003@gmail.com>
2023-02-03 18:23:01 -0300

    added game loop
    this was the ending of the guessing game tutorial

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,25 +6,34 @@ fn print_game_heading() {
     println!("Welcome player, lets play the guessing game.");
     println!("The game works just like this");
     println!("first the game generates a number between 1 and 100");
-    println!("then you will be asket to input a number, if you hit the number generated you win");
+    println!("then you will be asked to input a number, if you hit the number generated you win");
 }
 
 fn main() {
     print_game_heading();
 
     let secret_number = rand::thread_rng().gen_range(1..101);
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
 
-    let result = io::stdin().read_line(&mut guess);
-    result.expect("Something went wring while trying to read the input");
+        let result = io::stdin().read_line(&mut guess);
+        result.expect("Something went wring while trying to read the input");
 
-    let guess: u8 = guess.trim().parse().expect("Please type a number between 1 and 100!");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Equal => println!("You win!!!"),
-        Ordering::Less => println!("Smaller!"),
-        Ordering::Greater => println!("Bigger!"),
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("You win!!!");
+                break;
+            },
+            Ordering::Less => println!("Smaller!"),
+            Ordering::Greater => println!("Bigger!"),
+        }
     }
-    
-    println!("You guessed: {guess}");
 }
